bd48ab39773a7b0afbac157d85861ed7cac8d423
Chris <chris.campisi@icloud.com>
2014-09-25 16:47:25 -0700

    Updated first function

--- Desktop/Project4/campisi_christopher_project4.js
+++ Desktop/Project4/campisi_christopher_project4.js
@@ -3,3 +3,23 @@
 //Project 4
 
 //Function 1
+currency();
+
+//Function 1 of the number list
+function currency()
+{
+	var number = 100;
+	var number2 = 50.235;
+	console.log("The number is: " + number);
+
+	number = number.toFixed(2);
+	console.log("The number as currency is: $" + number);
+	
+	number = parseFloat(number).toFixed(3);
+	console.log("The number with 3 decimal spots is: " + number);
+	
+	console.log("Second number is: " + number2);
+	number2 = number2.toFixed(2);
+	
+	console.log("Number shortened to: " + number2);
+}
